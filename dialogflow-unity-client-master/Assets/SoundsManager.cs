578a6a8671a464bf44dc31d9c38d5c125d49393b
mameya007 <mameya.mseddi@gmail.com>
2017-12-03 13:41:49 +0100

    mm

---
+++ dialogflow-unity-client-master/Assets/SoundsManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsManager : MonoBehaviour {
+
+    public AudioSource audiosour;
+    public AudioClip win;
+    public AudioClip lose;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Win()
+    {
+        audiosour.clip = win;
+        audiosour.Play();
+    }
+    public void Lose()
+    {
+        audiosour.clip = lose;
+        audiosour.Play();
+    }
+}
